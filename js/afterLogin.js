53363f968550cc392c73b4a2de3f91647cce4a31
LZW-wr <19877522782@163.com>
2024-08-20 10:46:56 +0800

    Add files via upload

---
+++ js/afterLogin.js
@@ -0,0 +1,20 @@
+const span = document.querySelector(".user span")
+// 渲染函数，退出时也会用到
+function render() {
+    // 获取本地存储的用户数据
+    const uPhone = localStorage.getItem("go-phone")
+    // console.log(uPhone);
+    if (uPhone) {
+        span.innerHTML = `${uPhone}`
+    }
+    else {
+        span.innerHTML = `登录/注册`
+    }
+}
+render()
+
+// 退出登录
+// span.addEventListener("click", function () {
+//     localStorage.removeItem("uPhone")
+//     render()
+// })
